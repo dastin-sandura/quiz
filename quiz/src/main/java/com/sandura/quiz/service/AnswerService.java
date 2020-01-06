7e5974288f1944f944f05e68e940ccd15e9be4a3
DKuwalek <d.kuwalek@gmail.com>
2020-01-06 20:35:43 +0100

    Services for handling data model operations
     - QuestionService for reading data from DB and CSV file
     - AnswerService for reading data from CSV and saving to database

---
+++ quiz/src/main/java/com/sandura/quiz/service/AnswerService.java
@@ -0,0 +1,39 @@
+package com.sandura.quiz.service;
+
+import com.sandura.quiz.repository.AnswerRepository;
+import com.sandura.quiz.data.CsvFileDataReader;
+import com.sandura.quiz.model.Answer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+
+@Service
+public class AnswerService {
+
+    @Autowired
+    AnswerRepository answerRepository;
+
+    @Autowired
+    CsvFileDataReader csvFileDataReader;
+
+    private static final Logger log = LoggerFactory.getLogger(AnswerService.class);
+
+    public void readAndPersistAnswers(File answersCsvFile) {
+        try {
+            List<Answer> answers = csvFileDataReader.readAnswersFromFile(answersCsvFile);
+            answerRepository.saveAll(answers);
+        } catch (FileNotFoundException fileNotFoundException) {
+            log.error("FileNotFoundException occurred while reading data from files.");
+            log.error(fileNotFoundException.toString());
+        } catch (Exception e) {
+            log.error("Exception occurred while reading from files.");
+            log.error(e.toString());
+        }
+    }
+
+}
---
+++ quiz/src/main/java/com/sandura/quiz/service/QuestionService.java
@@ -0,0 +1,53 @@
+package com.sandura.quiz.service;
+
+import com.sandura.quiz.repository.QuestionRepository;
+import com.sandura.quiz.data.CsvFileDataReader;
+import com.sandura.quiz.model.Question;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service
+public class QuestionService {
+
+    private static final Logger log = LoggerFactory.getLogger(QuestionService.class);
+
+    @Autowired
+    QuestionRepository questionRepository;
+
+    @Autowired
+    CsvFileDataReader csvFileDataReader;
+
+    public Set<Question> getQuestionsByCategory(int questionsCount, ArrayList<String> selectedCQuestionCategories) {
+        Set<Question> onlyTesting = new HashSet<>();
+        for (Question question : questionRepository.findAll()) {
+            if (selectedCQuestionCategories.contains(question.getCategory()) && questionsCount > 0) {
+                onlyTesting.add(question);
+                questionsCount -= 1;
+            }
+        }
+        return onlyTesting;
+    }
+
+    public void readAndPersistQuestions(File questionsCsvFile) {
+        try {
+            List<Question> questions = csvFileDataReader.readQuestionsFromFile(questionsCsvFile);
+            questionRepository.saveAll(questions);
+        } catch (FileNotFoundException fileNotFoundException) {
+            log.error("FileNotFoundException occurred while reading data from files.");
+            log.error(fileNotFoundException.toString());
+        } catch (Exception e) {
+            log.error("Exception occurred while reading from files.");
+            log.error(e.toString());
+        }
+    }
+
+}
