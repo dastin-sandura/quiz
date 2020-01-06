f53f682aac1ea85d5369e89df2f8f658e9adfc0e
DKuwalek <d.kuwalek@gmail.com>
2020-01-06 20:34:18 +0100

    Enum for available categories
     - QuestionCategoryEnum for making sure that only valid question category is used

---
+++ quiz/src/main/java/com/sandura/quiz/question/QuestionCategoryEnum.java
@@ -0,0 +1,14 @@
+package com.sandura.quiz.question;
+
+public enum QuestionCategoryEnum {
+
+    Testing("Testing"),
+
+    Java("Java");
+
+    String category;
+
+    QuestionCategoryEnum(String category) {
+        this.category = category;
+    }
+}
