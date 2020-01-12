da4090430b3b75299b8b32c996b0d32d8e09e49c
DKuwalek <d.kuwalek@gmail.com>
2020-01-12 18:56:36 +0100

    Update StarterDatasetImporter.java
     - removed the @Component from the interface to keep only one implementation and not confuse IntelIJ Idea

--- quiz/src/main/java/com/sandura/quiz/data/StarterDatasetImporter.java
+++ quiz/src/main/java/com/sandura/quiz/data/StarterDatasetImporter.java
@@ -1,8 +1,5 @@
 package com.sandura.quiz.data;
 
-import org.springframework.stereotype.Component;
-
-@Component
 public interface StarterDatasetImporter {
 
     void populateDatabaseWithStarterDataset();
