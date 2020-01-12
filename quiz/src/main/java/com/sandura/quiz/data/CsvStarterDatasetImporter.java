e88e4e41ede7242570c2ba66ba091f3a4035d40e
DKuwalek <d.kuwalek@gmail.com>
2020-01-12 18:56:55 +0100

    Update CsvStarterDatasetImporter.java
     - added logging for starting and finishing the import process

--- quiz/src/main/java/com/sandura/quiz/data/CsvStarterDatasetImporter.java
+++ quiz/src/main/java/com/sandura/quiz/data/CsvStarterDatasetImporter.java
@@ -2,6 +2,8 @@ package com.sandura.quiz.data;
 
 import com.sandura.quiz.service.AnswerService;
 import com.sandura.quiz.service.QuestionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -10,6 +12,8 @@ import java.io.File;
 
 @Component
 public class CsvStarterDatasetImporter implements StarterDatasetImporter {
+
+    Logger log = LoggerFactory.getLogger(CsvStarterDatasetImporter.class);
 
     @Value("${question.data.csv.relative-file-path}")
     private String QUESTIONS_CSV_FILE_DIRECTORY;
@@ -24,11 +28,14 @@ public class CsvStarterDatasetImporter implements StarterDatasetImporter {
     private AnswerService answerService;
 
     public void populateDatabaseWithStarterDataset() {
+        log.info("Started populating database with CSV data.");
         File questionsCsvFile = new File(QUESTIONS_CSV_FILE_DIRECTORY);
         File answersCsvFile = new File(ANSWERS_CSV_FILE_DIRECTORY);
 
         questionService.readAndPersistQuestions(questionsCsvFile);
         answerService.readAndPersistAnswers(answersCsvFile);
+        log.info("Populating database with CSV data finished.");
+
     }
 
 }
