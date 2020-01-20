7089c11a118c3aa4d0b75c36791e1d99c0bc8cfd
DKuwalek <d.kuwalek@gmail.com>
2020-01-20 19:20:31 +0100

    AnswerController API
     - add endpoint for checking all answers in the database

--- quiz/src/main/java/com/sandura/quiz/controller/AnswerController.java
+++ quiz/src/main/java/com/sandura/quiz/controller/AnswerController.java
@@ -1,9 +1,30 @@
 package com.sandura.quiz.controller;
 
+import com.sandura.quiz.model.Answer;
+import com.sandura.quiz.repository.AnswerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping(path = "answer")
 public class AnswerController {
+
+    @Autowired
+    private AnswerRepository answerRepository;
+
+    @GetMapping
+    public ResponseEntity<List<Answer>> getAllAnswers() {
+        List<Answer> allAnswers = new ArrayList<>();
+        for (Answer answer : answerRepository.findAll()) {
+            allAnswers.add(answer);
+        }
+        return new ResponseEntity<>(allAnswers, HttpStatus.OK);
+    }
 }
