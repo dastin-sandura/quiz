459b71e7529be337bcb971875fc6556b1356b748
DKuwalek <d.kuwalek@gmail.com>
2020-02-05 19:04:24 +0100

    Update QuizApplicationTests.java
     - add test for the main page

--- quiz/src/test/java/com/sandura/quiz/QuizApplicationTests.java
+++ quiz/src/test/java/com/sandura/quiz/QuizApplicationTests.java
@@ -1,13 +1,34 @@
 package com.sandura.quiz;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
 @SpringBootTest
 class QuizApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private MockMvc mvc;
+
+    private final Logger log = LoggerFactory.getLogger(QuizApplicationTests.class);
+
+    @Test
+    void contextLoads() {
+        log.info("Context loads test");
+    }
+
+    @Test
+    public void getMainPage() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk());
+    }
 
 }
