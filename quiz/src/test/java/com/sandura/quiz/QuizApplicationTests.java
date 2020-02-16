cf4a6e663547c734deb9e2e487f36aea794d97e3
DKuwalek <d.kuwalek@gmail.com>
2020-02-16 17:32:44 +0100

    Fix test of Quiz endpoint
     - add cleanup for H2 database

--- quiz/src/test/java/com/sandura/quiz/QuizApplicationTests.java
+++ quiz/src/test/java/com/sandura/quiz/QuizApplicationTests.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -20,6 +21,9 @@ class QuizApplicationTests {
 
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     @Autowired
     private CrudQuizRepository crudQuizRepository;
@@ -51,7 +55,8 @@ class QuizApplicationTests {
 
         //Cleanup
         crudQuizRepository.deleteAll();
+        //Reset sequence, because it used by every table. Without the reset, import of starter data set will fail.
+        jdbcTemplate.execute("ALTER SEQUENCE HIBERNATE_SEQUENCE restart with 1");
     }
-
 
 }
